9056941cf46774b78ffbba82b985a6288197b6f9
strawberrypicker01 <spec2lima@gmail.com>
2025-04-09 17:31:33 -0400

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,9 @@
+score1 = 50
+score2 = 53
+score3 = 54
+score4 = 55
+score5 =56
+sum = score1 + score2 + score3 + score4 + score5
+print("The sum of all the 5 variables: ",sum)
+average = sum/5
+print ("The average of all the 5 members is: ", average)
