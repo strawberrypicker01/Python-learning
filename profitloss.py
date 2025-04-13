1de84dedfcfccb9fd21870ea61a2551b5f5a30b4
strawberrypicker01 <spec2lima@gmail.com>
2025-04-13 16:27:40 -0400

    Add files via upload

---
+++ profitloss.py
@@ -0,0 +1,9 @@
+actual_cost = float(input("Enter the actual cost: "))
+selling_price = float(input("Enter the selling price: "))
+profit = selling_price - actual_cost
+if profit > 0:
+    print("Profit is: ", profit)
+elif profit > 0:
+    print("Loss is: ",Loss)
+else:
+    print("No profit no loss")
