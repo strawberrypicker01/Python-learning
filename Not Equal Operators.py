8d4ecffe2918bd095676d194fb7a3b166ed55e9b
strawberrypicker01 <spec2lima@gmail.com>
2025-04-20 16:19:15 -0400

    Add files via upload

---
+++ Not Equal Operators.py
@@ -0,0 +1,16 @@
+a = 10
+b = 12
+c = 12
+print(a!= b)
+print(b != c)
+a= "Python"
+b = "Coding"
+if a!=b:
+    print(a, "and", b, "are not equal")
+a = 4
+b = 5
+if (a==1) != (b==5):
+    print ('True')
+a = int(input("Enter a number: "))
+if a%2 != 0:
+    print('The number is odd')
