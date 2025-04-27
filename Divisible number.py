1a5b998e0a84b574462a3594881ff103d696c8bf
strawberrypicker01 <spec2lima@gmail.com>
2025-04-27 16:26:12 -0400

    Add files via upload

---
+++ Divisible number.py
@@ -0,0 +1,16 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+
+#sum of 40 numbers
+sum=mean1*total_number
+print("The sum of 40 numbers:" ,sum)
+
+#correct sum of these numbers
+SUM2=sum-(wrong_number)+(correct_number)
+print("Corrected Sum", SUM2)
+
+#the correct mean
+mean2=SUM2/total_number
+print(mean2)
