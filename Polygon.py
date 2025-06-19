8f366aa685ce8b2c73a6936b8bde5baef240fcd7
strawberrypicker01 <spec2lima@gmail.com>
2025-06-18 20:25:54 -0400

    Add files via upload

--- Polygon.py
+++ Polygon.py
@@ -1,10 +1,12 @@
 import turtle
 
 screen = turtle.Screen()
-screen.setup(1.0, 1.0)
-
-t = turtle.Turtle()
-for c in ['red', 'green', 'blue', 'yellow']:
-    t.color(c)
-    t.forward(75)
-    t.left(90)
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+num_sides = 6
+side_length = 70
+angle = 360.0/num_sides
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
