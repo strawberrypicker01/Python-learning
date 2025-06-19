74dec3e688a3d123b93119d04257e815c0efb449
strawberrypicker01 <spec2lima@gmail.com>
2025-06-18 20:17:36 -0400

    Add files via upload

---
+++ Polygon.py
@@ -0,0 +1,10 @@
+import turtle
+
+screen = turtle.Screen()
+screen.setup(1.0, 1.0)
+
+t = turtle.Turtle()
+for c in ['red', 'green', 'blue', 'yellow']:
+    t.color(c)
+    t.forward(75)
+    t.left(90)
